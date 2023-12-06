f0c45cfb597316289dd894f67ed75bf7b60a56de
madakelet <mileaddy@gmail.com>
2023-12-06 21:34:18 +0100

    added login form fields

--- resources/js/pages/Default/LoginPage.vue
+++ resources/js/pages/Default/LoginPage.vue
@@ -1,7 +1,31 @@
 <template>
-    <h1>
-        Login Page
-    </h1>
+    <section id="login">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-10 my-auto">
+                    <div class="mb-3 w-50 mx-auto">
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="e-mail"
+                        />
+                    </div>
+                    <div class="mb-3 w-50 mx-auto">
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="jelszó"
+                        />
+                    </div>
+                    <div class="mb-3 w-50 mx-auto text-center">
+                        <button class="text-center">
+                            bejelentkezés
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
 export default {
