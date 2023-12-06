4a375335566b89e103371decde926d2c9d9c4246
madakelet <mileaddy@gmail.com>
2023-12-06 21:04:17 +0100

    added font awesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,16 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import router from './router/router';
-import App from './App.vue';
+import "./bootstrap";
+import { createApp } from "vue";
+import router from "./router/router";
+import App from "./App.vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(fab, fas, far);
 
 const app = createApp({
     extends: App,
 });
-app
-    .use(router)
-    .mount('#app');
+app.component("font-awesome-icon", FontAwesomeIcon).use(router).mount("#app");
