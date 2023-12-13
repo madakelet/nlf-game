c05de22ee93da3720bc72e57109ff84dd0978392
madakelet <mileaddy@gmail.com>
2023-12-13 07:43:14 +0100

    awaiting images to load

--- resources/js/components/MatchComponent.vue
+++ resources/js/components/MatchComponent.vue
@@ -5,7 +5,8 @@
         </div>
         <div class="row match-component py-2">
             <div class="col-1 my-auto text-center">
-                <img :src="match.home_team.logo_url" class="img-fluid" />
+                <img :src="match.home_team.logo_url" class="img-fluid" v-if="imagesLoaded"/>
+                <loading-spinner v-else />
             </div>
             <div class="col-4 my-auto text-center">
                 <div class="team-name">
@@ -27,12 +28,15 @@
                 </small>
             </div>
             <div class="col-1 my-auto text-center">
-                <img :src="match.away_team.logo_url" class="img-fluid" />
+                <img :src="match.away_team.logo_url" class="img-fluid" v-if="imagesLoaded"/>
+                <loading-spinner v-else />
             </div>
         </div>
     </div>
 </template>
 <script>
+import { onMounted, ref } from 'vue';
+
 export default {
     name: "MatchComponent",
     props: {
@@ -40,6 +44,28 @@ export default {
             type: Object,
             required: true,
         },
+    },
+    setup(props) {
+        const imagesLoaded = ref(false);
+        const checkIfImagesLoaded = () => {
+            const imgHome = new Image();
+            imgHome.src = props.match.home_team.logo_url;
+            const imgAway = new Image();
+            imgAway.src = props.match.away_team.logo_url;
+            imgHome.onload = () => {
+                imgAway.onload = () => {
+                    console.log("loaded");
+                    imagesLoaded.value = true;
+                };
+            };
+        };
+
+        onMounted(() => {
+            checkIfImagesLoaded();
+        });
+        return {
+            imagesLoaded,
+        };
     },
 };
 </script>
